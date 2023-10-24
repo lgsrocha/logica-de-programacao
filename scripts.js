f53d32a89fafc54db6f7a3d16edecd878cc5041e
samirls <samirlaguardiaii@gmail.com>
2023-10-24 00:26:30 -0300

    front end renderizando lista de tarefas

--- scripts.js
+++ scripts.js
@@ -1,9 +1,132 @@
-const { tarefas } = require("./todoList");
+const tarefas = [
+  {
+    id: 0,
+    descricao: "Comprar pão",
+    dataDeInicio: "19/10/2023, 23:26:05",
+    dataDeConclusao: null,
+    status: false,
+  },
+  {
+    id: 1,
+    descricao: "Comprar leite",
+    dataDeInicio: "01/09/2023, 10:10:30",
+    dataDeConclusao: null,
+    status: false,
+  },
+  {
+    id: 42,
+    descricao: "Comprar manteiga",
+    dataDeInicio: "08/07/2023, 18:45:37",
+    dataDeConclusao: null,
+    status: false,
+  },
+  {
+    id: 2,
+    descricao: "Comprar café",
+    dataDeInicio: "28/08/2023, 14:26:33",
+    dataDeConclusao: null,
+    status: false,
+  },
+];
+
+function novaTarefa(descricao) {
+  let maiorId = tarefas.reduce((a, b) => (b.id > a ? b.id : a), 0);
+  let id = maiorId + 1;
+  let dataDeInicio = new Date().toLocaleString("pt-BR");
+  let dataDeConclusao = null;
+  let status = false;
+  let tarefa = { id, descricao, dataDeInicio, dataDeConclusao, status };
+  try {
+    tarefas.push(tarefa);
+    console.log(`tarefa adicionada com sucesso, ID: ${id}`);
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+function lerTarefas(tarefas) {
+  if (tarefas.length === 0) {
+    console.log("Lista de Tarefas em Branco!");
+    return false;
+  } else {
+    console.log("Tarefas Cadastradas: ");
+    for (const tarefa of tarefas) {
+      console.log("ID: ", tarefa.id);
+      console.log("Descrição: ", tarefa.descricao);
+      console.log("Data de Início: ", tarefa.dataDeInicio);
+      if (tarefa.dataDeConclusao === null) {
+        console.log("Data de Conclusão: Pendente");
+      } else {
+        console.log("Data de Conclusão: ", tarefa.dataDeConclusao);
+      }
+      console.log("Status: ", tarefa.status ? "Concluída" : "Pendente");
+      console.log("*".repeat(15));
+    }
+  }
+}
+
+lerTarefas(tarefas);
+
+function editarTarefaPorId(tarefas, id, novaDescricao, novoStatus) {
+  const tarefaIndex = tarefas.findIndex((tarefa) => tarefa.id === parseInt(id));
+  if (tarefaIndex !== -1) {
+    tarefas[tarefaIndex].descricao = novaDescricao;
+    if (novoStatus) {
+      tarefas[tarefaIndex].status = true;
+      tarefas[tarefaIndex].dataDeConclusao = new Date().toLocaleString();
+    }
+  } else {
+    console.log("Tarefa não encontrada com o ID fornecido.");
+  }
+}
+
+function editarTarefaPorDescricao(
+  tarefas,
+  descricao,
+  novaDescricao,
+  novoStatus
+) {
+  const tarefa = tarefas.find(
+    (tarefa) => tarefa.descricao.toLowerCase() === descricao.toLowerCase()
+  );
+  if (tarefa) {
+    tarefa.descricao = novaDescricao;
+    if (novoStatus) {
+      tarefa.status = true;
+      tarefa.dataDeConclusao = new Date().toLocaleString();
+    }
+  } else {
+    console.log("Tarefa não encontrada com a descrição fornecida.");
+  }
+}
+
+function deletarTarefa(tarefas, id) {
+  const tarefaIndex = tarefas.findIndex((item) => item.id === id);
+
+  if (tarefaIndex !== -1) {
+    tarefas.splice(tarefaIndex, 1);
+    console.log("Tarefa excluída com sucesso!");
+  } else {
+    console.log("Tarefa não encontrada!");
+  }
+}
+
+function pesquisarTarefa(tarefas, pesquisa) {
+  const tarefasEncontradas = tarefas.filter((tarefa) =>
+    tarefa.descricao.toLowerCase().includes(pesquisa.toLowerCase())
+  );
+
+  lerTarefas(tarefasEncontradas);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 
 const conteudo = document.querySelector(".conteudo");
 const ID = document.getElementById("ID");
-
-const lista = [];
 
 function criar_tabela() {
   conteudo.innerHTML = "";
@@ -22,11 +145,11 @@ function criar_tabela() {
   thc2.innerHTML = "Descrição";
   thc3.innerHTML = "Data de Início";
   thc4.innerHTML = "Data de Conclusão";
-  thc5.innerHTML = "Status";
+  thc5.innerHTML = "Estado";
 
   thlinha.classList.add("thlinha");
 
-  for (let contador = 0; contador < lista_pessoas.length; contador++) {
+  for (let contador = 0; contador < tarefas.length; contador++) {
     let linha = tabela.insertRow();
     let ID = linha.insertCell(0);
     let descricao = linha.insertCell(1);
@@ -38,7 +161,7 @@ function criar_tabela() {
     descricao.innerHTML = tarefas[contador].descricao;
     dataInicio.innerHTML = tarefas[contador].dataDeInicio;
     dataConclusao.innerHTML = tarefas[contador].dataDeConclusao;
-    status.innerHTML = tarefas[contador].status;
+    status.innerHTML = tarefas[contador].status === false ? 'A fazer' : 'Feito';
 
     if (contador >= 0) {
       ID.classList.add("celula-td");
@@ -52,3 +175,5 @@ function criar_tabela() {
 
   conteudo.style.display = "block";
 }
+
+criar_tabela();
